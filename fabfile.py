d7f602f1e0ca3cdd855587134d60757da7f5fc37
Nate Beaty <nate@clixel.com>
2022-03-21 23:24:34 -0400

    basic fabfile for deploys

---
+++ fabfile.py
@@ -0,0 +1,39 @@
+from fabric import task
+from invoke import run as local
+
+remote_path = "/home/natebeaty/apps/al6"
+remote_hosts = ["natebeaty@natebeaty.opalstacked.com"]
+php_command = "php74"
+
+# set to production
+# @task
+# def production(c):
+#     global remote_hosts, remote_path
+#     remote_hosts = ["natebeaty@natebeaty.com"]
+#     remote_path = "/home/natebeaty/apps/nb-craft3-production"
+
+# deploy
+@task(hosts=remote_hosts)
+def deploy(c):
+    update(c)
+    composer_update(c)
+    clear_cache(c)
+
+def update(c):
+    c.run("cd {} && git pull".format(remote_path))
+
+def composer_update(c):
+    c.run("cd {} && {} ~/bin/composer.phar install".format(remote_path, php_command))
+
+def clear_cache(c):
+    c.run("cd {} && ./craft clear-caches/compiled-templates".format(remote_path))
+    c.run("cd {} && ./craft clear-caches/data".format(remote_path))
+
+# local commands
+# @task
+# def assets(c):
+#     local("gulp --production")
+
+# @task
+# def dev(c):
+#     local("gulp watch")
