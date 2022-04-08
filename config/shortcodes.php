e6cf29903a0c1d8551b31b7faeec349d60b9bd2c
Nate Beaty <nate@clixel.com>
2022-04-08 15:55:38 -0400

    add link blocks field to comics index

--- config/shortcodes.php
+++ config/shortcodes.php
@@ -1,7 +1,7 @@
 <?php
 return [
     'map' => [
-        'social-links' => 'partials/_social-links.twig',
-        'contact' => 'partials/_contact.twig',
+        'social-links' => 'shared/_social-links.twig',
+        'contact' => 'shared/_contact.twig',
     ]
 ];
